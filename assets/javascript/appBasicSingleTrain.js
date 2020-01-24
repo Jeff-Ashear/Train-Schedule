74cd7785d980b9de4961dcb24800b777ccf2f693
Jeff.A <kinesthesiac@gmail.com>
2020-01-24 14:16:10 -0700

    finalized inital math and variables.  made arrays for the working app.

--- assets/javascript/appBasicSingleTrain.js
+++ assets/javascript/appBasicSingleTrain.js
@@ -26,39 +26,3 @@ console.log("minutes till arrival: " + t1MinutesTillArrival)
 var t1NextArrival = moment().add(t1MinutesTillArrival, "minutes");
 // moment(t1NextArrival).format("HH:mm");
 console.log("next arrival time: " + moment(t1NextArrival).format("HH:mm"))
-
-
-//4 arrays for names, destinations, frequency, and first arrivals
-var trainNames = [
-    "Ossious Eyes"
-    "Squee Biscuit"
-    "Tandy Pine"
-    "C6, C6, D9#11, %, Dm7, G7"
-];
-
-var destinations = [
-    "The Underverse",
-    "Booklyn",
-    "Post Appocalypse",
-    "Ellingtonia"
-];
-
-//frequencies array is expressed in minutes
-var frequencies = [
-    7,
-    13,
-    5,
-    4,
-];
-
-//firstArrivals is expressed in HH:mm
-var firstArrivals = [
-    "00:01",
-    "03:18",
-    "1:00",
-    "04:04",
-];
-
-//function which loops each arrray, does the math, and appends to the table.
-
-//onclick function to capture the form input, add the inputs to the arrays, clears the table, and then calls the first function
